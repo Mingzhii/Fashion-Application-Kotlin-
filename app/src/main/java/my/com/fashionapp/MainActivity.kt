a17baefd9343cb152554b37e8c6236b80c0c2bed
Mingzhii <87804524+Mingzhii@users.noreply.github.com>
2021-08-22 23:35:40 +0800

    Update MainActivity.kt

--- app/src/main/java/my/com/fashionapp/MainActivity.kt
+++ app/src/main/java/my/com/fashionapp/MainActivity.kt
@@ -2,10 +2,11 @@ package my.com.fashionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import my.com.fashionapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     
-    private lateinit var binding = ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
